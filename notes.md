941c3c4fd6b7fbc1018d1ebc8cfc745025a41272
Gregory Reburn <greg@reburn.io>
2024-04-04 19:47:59 -0500

    checkpoint

---
+++ notes.md
@@ -0,0 +1,17 @@
+# current state
+
+This will use intelisense to suggest subjects based on the current prefix. For example, rdf: will suggest subjects from the rdf namespace, as it is defined in the current file.
+
+# next steps
+
+look at context of current line to determine which terms to suggest. Is it a subject, predicate, or object? if object, what is the range? if predicate, what is allowed for that subject?
+
+# how to run
+
+Press F5, that should open a new vscode window.. open the ~/src/example.ttl file in that window. Find a line that says rdf:... and attempt to engage the intellisense. You should see term1, term2, term3 from the namespace. Ignore the TypeScript error on provideCompletionItems method. This is not important/issue with Thenable/Promises. Its just a "contract" issue but I prefer to use the suggested typescript return type which came from https://code.visualstudio.com/api#get-started-articles
+
+# TODO / Features
+
+Create a publisher identity and publish v1 of the extension - https://code.visualstudio.com/api/working-with-extensions/publishing-extension#create-a-publisher
+
+infer more context: which part of the tripple is the user? Is this the Object, Subject or predicate? If a predicate, the scope can be limited to the range of the subject. As for the object, could you somehow query for valid ids? Would this be helpful/make sense in the context of vocabulary definition as well as when defining graphs?
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,128 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import rdfParser from "rdf-parse";
+import { Readable } from "stream";
+import { QueryEngine } from "@comunica/query-sparql";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "rdflang" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand("rdflang.helloWorld", () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage("Hello World from rdflang! GMR");
-	});
+	// Register completion item provider
+	context.subscriptions.push(
+		vscode.languages.registerCompletionItemProvider(
+			{ language: "turtle", scheme: "file" }, // "rdf" for RDF documents
+			new RDFCompletionItemProvider(),
+			":" // Trigger character(s) for activating IntelliSense
+		)
+	);
+}
+// Query for all subjects defined in the RDF data
+async function querySubjectsFromIRI(
+	iri: string
+): Promise<{ label: string; description: string }[]> {
+	try {
+		const comunicaEngine = new QueryEngine();
 
-	context.subscriptions.push(disposable);
+		// Construct SPARQL query
+		const query = `
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT ?s ?label ?description
+            WHERE {
+                ?s ?p <${iri}>.
+                OPTIONAL { ?s rdfs:label ?label. }
+                OPTIONAL { ?s rdfs:comment ?description. }
+            } LIMIT 100`;
+
+		// Execute SPARQL query
+		const bindingsStream = await comunicaEngine.queryBindings(query, {
+			sources: [iri],
+		});
+
+		// Store results
+		const subjects: { label: string; description: string }[] = [];
+
+		// Extract labels and descriptions from query results
+		for await (const binding of bindingsStream) {
+			const label = binding.has("label") ? binding.get("label")!.value : "";
+			const description = binding.has("description")
+				? binding.get("description")!.value
+				: "";
+			subjects.push({ label, description });
+		}
+
+		return subjects;
+	} catch (error) {
+		console.error("Error querying subjects from IRI:", error);
+		throw error;
+	}
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+class RDFCompletionItemProvider implements vscode.CompletionItemProvider {
+	async provideCompletionItems(
+		document: vscode.TextDocument,
+		position: vscode.Position,
+		token: vscode.CancellationToken,
+		context: vscode.CompletionContext
+		// @ts-ignore
+	): vscode.ProviderResult<
+		vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>
+	> {
+		const completionItems: vscode.CompletionItem[] = [];
+		// Create a readable stream from the RDF data
+		const stream = Readable.from(document.getText());
+
+		// Create a new RDF parser
+		const parser = rdfParser.parse(stream, {
+			contentType: "text/turtle",
+			baseIRI: document.uri.toString(),
+		});
+
+		// Collect prefixes and corresponding IRIs
+		const prefixes: { [key: string]: string } = {};
+		parser.on("prefix", (prefix, iri) => {
+			prefixes[prefix] = iri.value;
+		});
+
+		// Await for parsing to complete
+		await new Promise<void>((resolve, reject) => {
+			parser.on("error", reject);
+			parser.on("data", resolve);
+		});
+
+		var wordRange = document.getWordRangeAtPosition(position);
+		const phrase = document.getText(wordRange);
+		const words = phrase.split(":");
+		const prefix = words[0];
+		if (prefixes[prefix] !== undefined) {
+			const subjects = await querySubjectsFromIRI(prefixes[prefix]);
+			subjects.forEach((subject) => {
+				const completionItem = createCompletionItem(
+					`${prefix}:${subject.label}`,
+					subject.description
+				);
+				completionItems.push(completionItem);
+			});
+		}
+
+		return completionItems;
+	}
+}
+
+function createCompletionItem(
+	label: string,
+	detail: string
+): vscode.CompletionItem {
+	const completionItem = new vscode.CompletionItem(label);
+	completionItem.detail = detail;
+	completionItem.kind = vscode.CompletionItemKind.Variable;
+	completionItem.documentation = new vscode.MarkdownString(
+		`Inserts "${label}" - ${detail}`
+	);
+	return completionItem;
+}
