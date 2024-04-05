e83e7da8d5011fd1553edaa56c92a8ed17c1a341
Gregory Reburn <greg@reburn.io>
2024-04-04 19:51:29 -0500

    update name

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ import { QueryEngine } from "@comunica/query-sparql";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "rdflang" is now active!');
+	console.log('Congratulations, your extension "turtlesense" is now active!');
 
 	// Register completion item provider
 	context.subscriptions.push(
